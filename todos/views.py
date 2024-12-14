21e4ede2b1b16d8a75d3831061fc5f3e62d0e954
caiostw <engsoftwaresaquarema@gmail.com>
2024-12-14 00:42:18 -0300

    Treino Recuperação

---
+++ todos/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView
+from .models import Todo
+
+
+class TodoListView(ListView):
+    model = Todo
+
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
